b412f90bc851b107823a2d6c4133fd8241cd32e1
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-12-28 14:14:55 +0100

    I added more commands for scaning wifi

--- scaner.py
+++ scaner.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import subprocess
 
 def run_airmon():
-    # Uruchamia polecenie 'airmon-ng start wlan1'
+    # Uruchamia polecenie 'sudo airmon-ng start wlan1'
     process = subprocess.Popen(['airmon-ng', 'start', 'wlan1'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
     output.delete(1.0, tk.END)  # Czyści okno tekstowe przed nowym wyjściem
     for line in iter(process.stdout.readline, ''):
@@ -17,31 +17,88 @@ def run_iwconfig():
         output.insert(tk.END, line)  # Wyświetla dane w czasie rzeczywistym
         output.update()
 
+def run_network_manager():
+    # Uruchamia polecenie 'systemctl start NetworkManager'
+    process = subprocess.Popen(['systemctl', 'start', 'NetworkManager'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    output.delete(1.0, tk.END)  # Czyści okno tekstowe przed nowym wyjściem
+    for line in iter(process.stdout.readline, ''):
+        output.insert(tk.END, line)  # Wyświetla dane w czasie rzeczywistym
+        output.update()
+
+def run_airmon_check_kill():
+    # Uruchamia polecenie 'airmon-ng check kill'
+    process = subprocess.Popen(['airmon-ng', 'check', 'kill'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    output.delete(1.0, tk.END)  # Czyści okno tekstowe przed nowym wyjściem
+    for line in iter(process.stdout.readline, ''):
+        output.insert(tk.END, line)  # Wyświetla dane w czasie rzeczywistym
+        output.update()
+
+def run_airodump():
+    # Uruchamia polecenie 'airodump-ng wlan1'
+    process = subprocess.Popen(['airodump-ng', 'wlan1'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    output.delete(1.0, tk.END)  # Czyści okno tekstowe przed nowym wyjściem
+    for line in iter(process.stdout.readline, ''):
+        output.insert(tk.END, line)  # Wyświetla dane w czasie rzeczywistym
+        output.update()
+
 # Tworzenie głównego okna aplikacji
 app = tk.Tk()
 app.title('Airmon-NG GUI')
 
-# Przycisk do uruchamiania 'airmon-ng start wlan1'
-airmon_button = tk.Button(
+# Przycisk do uruchamiania 'sudo airmon-ng start wlan1'
+sudo_airmon_button = tk.Button(
     app,
     text='Start Airmon',
     command=run_airmon,
-    width=20,
+    width=25,
     height=2,
     font=('Helvetica', 16)
 )
-airmon_button.pack(pady=10)
+sudo_airmon_button.pack(pady=10)
 
 # Przycisk do uruchamiania 'iwconfig'
 iwconfig_button = tk.Button(
     app,
     text='Show IWConfig',
     command=run_iwconfig,
-    width=20,
+    width=25,
     height=2,
     font=('Helvetica', 16)
 )
 iwconfig_button.pack(pady=10)
+
+# Przycisk do uruchamiania 'systemctl start NetworkManager'
+network_manager_button = tk.Button(
+    app,
+    text='Start Network Manager',
+    command=run_network_manager,
+    width=25,
+    height=2,
+    font=('Helvetica', 16)
+)
+network_manager_button.pack(pady=10)
+
+# Przycisk do uruchamiania 'airmon-ng check kill'
+airmon_check_kill_button = tk.Button(
+    app,
+    text='Check Kill Airmon',
+    command=run_airmon_check_kill,
+    width=25,
+    height=2,
+    font=('Helvetica', 16)
+)
+airmon_check_kill_button.pack(pady=10)
+
+# Przycisk do uruchamiania 'airodump-ng wlan1'
+airodump_button = tk.Button(
+    app,
+    text='Start Airodump',
+    command=run_airodump,
+    width=25,
+    height=2,
+    font=('Helvetica', 16)
+)
+airodump_button.pack(pady=10)
 
 # Tworzenie okna tekstowego do wyświetlania wyników
 output = tk.Text(app, height=20, width=80, font=('Courier', 14))
